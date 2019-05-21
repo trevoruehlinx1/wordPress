afe3955e07614c598534deba647e0951c4909dce
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-05-21 08:01:45 -0700

    Making Progress

--- wp-content/themes/twentyseventeen/front-page.php
+++ wp-content/themes/twentyseventeen/front-page.php
@@ -59,28 +59,24 @@ get_header(); ?>
 		<div class="menuImage" id="menuImage">
 			<img class="theImg" src="wp-content\themes\twentyseventeen\assets\images\Capture.PNG" />
 		</div>
-		<h1 class="announcmentsLabel">Announcments</h1>
+		<h1 class="announcments-label">Announcments</h1>
 		<div class="trevor-posts-container">
 			<div class="trevor-posts" >
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
 					<?php
-					wp_reset_query();
-					$query = new WP_Query( array( 'post_type' => 'post' ) );
-					//$recent_posts = wp_get_recent_posts();
-
-
-					// Show the selected front page content.
-					if ($query->have_posts() ) :
-						while ( $query->have_posts() ) :
-							$query->the_post();
-							get_template_part( 'template-parts/page/content', 'front-page-trevor' );
-						endwhile;
-					else :
-						get_template_part( 'template-parts/post/content', 'none' );
-					endif;
+						wp_reset_query();
+						$query = new WP_Query( array( 'post_type' => 'post' ) );
+						// Show the selected front page content.
+						if ($query->have_posts() ) :
+							while ( $query->have_posts() ) :
+								$query->the_post();
+								get_template_part( 'template-parts/page/content', 'front-page-trevor' );
+							endwhile;
+						else :
+							get_template_part( 'template-parts/post/content', 'none' );
+						endif;
 					?>
-					
 				</div>
 			</div>
 		</div>
